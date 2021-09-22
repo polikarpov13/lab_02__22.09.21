7e05b7deeef241c20b157e7c8a41fba3dd1e4871
polikarpov13 <vladyslav.polikarpov.ik.2021@lpnu.ua>
2021-09-22 13:48:10 +0300

    first master commit

---
+++ LAB_2/LAB_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x;
+    double z1;
+    double z2;
+
+
+    cout << "X = ";
+    cin >> x;
+
+    z1 = (1 - 2 * pow(sin(x), 2)) / (1 + sin(2 * x));
+    z2 = (1 - tan(x))/(1 + tan(x));
+
+    cout << "Z1 = " << z1 << endl;
+    cout << "Z2 = " << z2 << endl;
+
+}
+
